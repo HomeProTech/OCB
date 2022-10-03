a0a21684ce01bc13e391693b803273014d637a5c
Billy Keys <b.keys@homeprotech.com>
2022-10-03 16:35:59 -0500

    Overrides

--- addons/stock_account/models/stock.py
+++ addons/stock_account/models/stock.py
@@ -703,8 +703,8 @@ class StockReturnPicking(models.TransientModel):
         new_picking = self.env['stock.picking'].browse([new_picking_id])
         for move in new_picking.move_lines:
             return_picking_line = self.product_return_moves.filtered(lambda r: r.move_id == move.origin_returned_move_id)
-            if return_picking_line and return_picking_line.to_refund:
-                move.to_refund = True
+            # if return_picking_line and return_picking_line.to_refund:
+            #     move.to_refund = True
         return new_picking_id, pick_type_id
 
 
